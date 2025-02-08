09a5fa866ddd47d8dc37387f239aae84cb7177d1
Neel-nufc <neelesh.nufc47@gmail.com>
2025-02-08 13:17:27 +0530

    combinations from 1 to n

---
+++ backtracking/combination.py
@@ -0,0 +1,21 @@
+
+def combine(n: int, k: int) :
+    res = []
+    
+    def combinations(start, comb):
+        if len(comb) == k:
+            res.append(comb[:])
+            return
+        
+        for i in range(start,n+1):
+            if i not in comb:
+                comb.append(i)
+                combinations(i,comb)
+                comb.pop()
+    
+    combinations(1,[])
+    return res
+
+print(combine(4,4))
+
+        
